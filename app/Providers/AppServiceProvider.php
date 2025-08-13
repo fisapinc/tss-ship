031a452133bb553b8bfd0c710afd1269148f4730
Fisa <75996452+NanaFisa@users.noreply.github.com>
2025-08-13 10:22:12 +0800

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+//use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setToStringFormat('d/m/Y');
+        \Carbon\Carbon::setLocale(config('app.locale'));
+        //Livewire::setLayout('layouts.app');
     }
 }
