ce7b07756292acc1883bd8ac302a9c4d8e8b58b5
Fisa <75996452+NanaFisa@users.noreply.github.com>
2025-08-13 10:22:48 +0800

    Add reportingmethod model to hostorical

--- app/Models/Historical.php
+++ app/Models/Historical.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Historical extends Model
 {
     protected $fillable = ['vessel_name', 'mmsi_number', 'call_sign', 'imo_number', 'draught', 'air_draught', 'total_person_onboard', 'flag', 'date_arrival', 'time_arrival', 'entry_sector', 'direction', 'position', 'port_destination', 'speed', 'course', 'vessel_type', 'imo_classes', 'hazardous_cargo', 'quantity', 'description', 'comments', 'rule_10', 'vessel_email', 'internal_remark' ];
+
+    public function reportingMethod()
+    {
+        return $this->belongsTo(\App\Models\ReportingMethod::class);
+    }
 }
